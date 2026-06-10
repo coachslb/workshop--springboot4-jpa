0c77b99b2fb411f31aff61af062d4ba27fe68507
José Garção <josegarcao1@gmail.com>
2026-06-10 16:25:55 +0100

    Exception handle - delete

--- src/main/java/com/coachslb/course/services/UserService.java
+++ src/main/java/com/coachslb/course/services/UserService.java
@@ -3,8 +3,11 @@ package com.coachslb.course.services;
 
 import com.coachslb.course.entities.User;
 import com.coachslb.course.repositories.UserRepository;
+import com.coachslb.course.services.exceptions.DatabaseException;
 import com.coachslb.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -30,7 +33,14 @@ public class UserService {
     }
 
     public void delete(Long id){
-        userRepository.deleteById(id);
+        try {
+            userRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e){
+            throw new DatabaseException(e.getMessage());
+        }
+
     }
 
     public User update(Long id, User userToUpdate) {
